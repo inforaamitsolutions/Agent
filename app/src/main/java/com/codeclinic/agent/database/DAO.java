6225d47423189df3280d0ba29b6c089efa657c29
jaypokar <jaypokarjdp@gmail.com>
2021-05-27 23:01:26 +0530

    validation changes

--- app/src/main/java/com/codeclinic/agent/database/DAO.java
+++ app/src/main/java/com/codeclinic/agent/database/DAO.java
@@ -7,10 +7,13 @@ import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import com.codeclinic.agent.database.business.BusinessDataFinalFormEntity;
+import com.codeclinic.agent.database.business.BusinessFormResumeEntity;
 import com.codeclinic.agent.database.customer.CustomerFinalFormEntity;
 import com.codeclinic.agent.database.customer.CustomerFormResumeEntity;
 import com.codeclinic.agent.database.lead.LeadFinalFormEntity;
+import com.codeclinic.agent.database.lead.LeadFormResumeEntity;
 import com.codeclinic.agent.database.supplier.SupplierFinalFormEntity;
+import com.codeclinic.agent.database.supplier.SupplierFormResumeEntity;
 import com.codeclinic.agent.model.businesDataUpdate.FetchBusinessDataFormBodyModel;
 import com.codeclinic.agent.model.customer.FetchCustomerFormBodyModel;
 import com.codeclinic.agent.model.customer.SaveCustomerFormEntries;
@@ -82,6 +85,18 @@ public interface DAO {
     @Delete
     void deleteLeadFinalForms(List<LeadFinalFormEntity> entity);
 
+    @Query("SELECT EXISTS(SELECT * FROM LeadFormResume)")
+    boolean isLeadFormResumeExists();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void saveLeadFormResume(LeadFormResumeEntity entity);
+
+    @Query("select * from LeadFormResume")
+    Flowable<LeadFormResumeEntity> getLeadFormResume();
+
+    @Delete
+    void removeLeadFormResume(LeadFormResumeEntity entity);
+
     /*****************Business Survey Form Table Queries*************************/
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -102,6 +117,18 @@ public interface DAO {
     @Delete
     void deleteBusinessDataFinalForms(List<BusinessDataFinalFormEntity> entity);
 
+    @Query("SELECT EXISTS(SELECT * FROM BusinessFormResume)")
+    boolean isBusinessFormResumeExists();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void saveBusinessFormResume(BusinessFormResumeEntity entity);
+
+    @Query("select * from BusinessFormResume")
+    Flowable<BusinessFormResumeEntity> getBusinessFormResume();
+
+    @Delete
+    void removeBusinessFormResume(BusinessFormResumeEntity entity);
+
     /*****************Supplier Survey Form Table Queries*************************/
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -121,6 +148,18 @@ public interface DAO {
 
     @Delete
     void deleteSupplierFinalForms(List<SupplierFinalFormEntity> entity);
+
+    @Query("SELECT EXISTS(SELECT * FROM SupplierFormResume)")
+    boolean isSupplierFormResumeExists();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void saveSupplierFormResume(SupplierFormResumeEntity entity);
+
+    @Query("select * from SupplierFormResume")
+    Flowable<SupplierFormResumeEntity> getSupplierFormResume();
+
+    @Delete
+    void removeSupplierFormResume(SupplierFormResumeEntity entity);
 
 
 }
